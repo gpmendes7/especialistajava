49d80a8f87b9bbf7f6f8bdf7a0b577fcbb5f70b8
gpmendes7 <andrewsseven@gmail.com>
2023-04-21 14:52:43 -0300

    source code

---
+++ cardapio/src/ItemCardapio.java
@@ -0,0 +1,13 @@
+public class ItemCardapio {
+
+    String descricao;
+    double preco;
+
+    boolean possuiPrecoEntre(double precoMinimo, double precoMaximo) {
+        return preco >= precoMinimo && preco <= precoMaximo;
+    }
+
+    void imprimir() {
+        System.out.printf("%s x R$.2fn", descricao, preco);
+    }
+}
