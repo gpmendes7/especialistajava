ad953b0e98697f029b71883704064ef3d40349f2
gpmendes7 <andrewsseven@gmail.com>
2023-06-01 16:21:17 -0300

    source code

---
+++ faturamento/src/com/algaworks/fiscal/GestorFiscal.java
@@ -0,0 +1,12 @@
+package com.algaworks.fiscal;
+
+public class GestorFiscal {
+
+    public void emitirNotasFiscais(NotaFiscal... notaFiscais) {
+        for (NotaFiscal notaFiscal: notaFiscais) {
+            notaFiscal.emitir();
+            System.out.println("-------------");
+        }
+    }
+
+}
