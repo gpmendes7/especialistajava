e45a7ef65f013cad2e9b5d9153a7d1c8ac458c90
gpmendes7 <andrewsseven@gmail.com>
2023-07-17 14:59:23 -0300

    source code

---
+++ date-time-api/src/Principal6.java
@@ -0,0 +1,10 @@
+import java.time.LocalTime;
+
+public class Principal6 {
+
+    public static void main(String[] args) {
+//        LocalTime horario = LocalTime.now();
+    LocalTime horario = LocalTime.of(23, 54, 10);
+        System.out.println(horario);
+    }
+}
